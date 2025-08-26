7e4071bf797eaa386d3c65cb00c58c849a0055f5
JatinAvtani <jatinavtani345@gmail.com>
2025-08-27 00:49:32 +0530

    backend attached and updated 003

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -8,7 +8,12 @@ import cors from "cors";
 
 const app = express();
 app.use(express.json());
-app.use(cors());
+
+app.use(cors({
+  origin: ['https://https://smart-brain-01-4nrp.vercel.app', 'http://localhost:3000'],
+  methods: ['GET','POST','PUT','DELETE']
+}));
+
 
 app.post("/api/v1/signup", async (req, res) => {
     // TODO: zod validation , hash the password
